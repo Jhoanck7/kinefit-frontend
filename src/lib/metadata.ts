085b902a439da35167d901bb9371bb8a566a797a
Jhoan Montero <jhoan.montero@alumnos.ucn.cl>
2026-07-08 13:22:51 -0400

    feat: link apple touch icon directly to existing Kinefit color.png and remove generation script

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -11,7 +11,7 @@ export const defaultMetadata: Metadata = {
   icons: {
     icon: '/icon.svg',
     shortcut: '/icon.svg',
-    apple: '/apple-touch-icon.png',
+    apple: '/Kinefit color.png',
   },
   creator: 'Kinefit',
   publisher: 'Kinefit',
