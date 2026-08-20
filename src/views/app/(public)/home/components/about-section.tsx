d66c1b186a6c9804c42d900177cd7fc39af3f399
Jhoanck7 <jhoan.montero@alumnos.ucn.cl>
2026-08-19 23:19:39 -0400

    Update about-section.tsx

--- src/views/app/(public)/home/components/about-section.tsx
+++ src/views/app/(public)/home/components/about-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-
+import Script from "next/script";
 import { LandingConfigResponse } from "@/models/responses";
 
 export default function AboutSection({
